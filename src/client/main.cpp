b9a5b83f9c826435ceeb683545031f63fab72078
javier2409 <javiferreyra97@gmail.com>
2019-10-11 04:50:35 -0300

    borro codigo del main del client

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -10,23 +10,5 @@ extern "C" {
 #define SUCCESS 0
 
 int main() {
-    /* LUA STUFF
-     */
-    auto L = luaL_newstate();
-    luaL_openlibs(L);
-    int err = luaL_loadfile(L, "plugins/luatest.lua");
-    if ((err) > 0){
-        std::cout << std::string(lua_tostring(L, -1)) << std::endl;
-    }
-    lua_pcall(L, 0, 0, 0);
-    lua_getglobal(L, "foo");
-    lua_pushnumber(L, (int)5);
-    lua_pushnumber(L, (int)6);
-    lua_pcall(L, 2, 1, 0);
-    auto x = (int)lua_tonumber(L, -1);
-    lua_pop(L, 1);
-
-    std::cout << "Lua function returned: " << x << std::endl;
-
     return SUCCESS;
 }
