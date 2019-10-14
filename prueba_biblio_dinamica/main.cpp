4b0a78749e4afd1fa12a75264c07ef6aab4bf85b
alejoacevedo <ale.acevedo@live.com.ar>
2019-10-14 11:14:37 -0300

    Use shared libs

--- prueba_biblio_dinamica/main.cpp
+++ prueba_biblio_dinamica/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-#include "gretting.h"
+#include <dlfcn.h>
+
+typedef int (*func_pointer)(void);
+
 
 int main(int argc, char *argv[]) {
-  Gretting gretting = Gretting();
-  std::cout << gretting.say_hello() << std::endl;
-  std::cout << gretting.say_goodbye() << std::endl;
+  const char *err;
+  void *shared_lib = dlopen("../gretting/build/libgretting.1.0.2.dylib", RTLD_NOW);
+  err = dlerror();
+  if (!shared_lib) {
+      printf("dlopen failed: %s\n", err);
+      return -1;
+  }
+  func_pointer by2;
+  *(void**) (&by2) = dlsym(shared_lib, "say_one");
+
+  std::cout << by2() << std::endl;
+  dlclose(shared_lib);
 }
