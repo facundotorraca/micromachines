341181935b84128f00d8ccc3a671f79b31e46dbc
alejoacevedo <ale.acevedo@live.com.ar>
2019-10-14 11:14:57 -0300

    Move gretting clas

--- prueba_biblio_dinamica/gretting.cpp
+++
@@ -1,9 +0,0 @@
-#include "gretting.h"
-
-Gretting::Gretting() : hello("HELLO WORD"), goodbye("GOODBYE WORLD") {}
-
-std::string Gretting::say_hello() { return this->hello; }
-
-std::string Gretting::say_goodbye() {return this->goodbye; }
-
-Gretting::~Gretting() {}
