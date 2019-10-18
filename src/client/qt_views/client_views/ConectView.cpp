9bc81b2098bd98f07573615ad7f38a103ddbb753
alejoacevedo <ale.acevedo@live.com.ar>
2019-10-18 19:57:05 -0300

    Add conectView

---
+++ src/client/qt_views/client_views/ConectView.cpp
@@ -0,0 +1,21 @@
+#include "ConectView.h"
+#include "ui_conectview.h"
+#include <iostream>
+#include <string>
+
+ConectView::ConectView(Socket &sck, QWidget *parent)
+                      : QDialog(parent), ui(), socket(sck) {
+  ui.setupUi(this);
+}
+
+void ConectView::on_btnBox_accepted() {
+  QPlainTextEdit *ipTextEdit = findChild<QPlainTextEdit*>("IPTxtIn");
+  QPlainTextEdit *serviceTextEdit = findChild<QPlainTextEdit*>("ServiceTxtIn");
+  std::string ipTxt = ipTextEdit->toPlainText().toStdString();
+  std::string serviceTxt = serviceTextEdit->toPlainText().toStdString();
+  std::cout<<ipTxt<<" ; "<<serviceTxt<<"\n";
+  this->socket.connect(ipTxt,serviceTxt);
+}
+
+ConectView::~ConectView() {
+}
