89989ba60454e90d3d2cf8606441208919739145
aleacevedo <ale.acevedo@live.com.ar>
2019-10-21 10:45:52 -0300

    Modify logic of joinview

--- qt_views/client_views/JoinView.cpp
+++ qt_views/client_views/JoinView.cpp
@@ -1,6 +1,7 @@
 #include <QListWidget>
 #include <QVector>
 #include <iostream>
+#include <QLineEdit>
 #include "JoinView.h"
 #include "ui_joinView.h"
 
@@ -9,7 +10,9 @@ void splitMatchs(std::string input, QStringList &match) {
   int last = 0;
   int next = input.find(delimiter);
   while (next != std::string::npos) {
-    match.push_back(QString::fromStdString(input.substr(last, next - last)));
+      QString match = QString::fromStdString(input.substr(last, next - last));
+      match
+    match.push_back(match);
     last = next +1;
     next = input.find_first_of(delimiter, last);
   }
@@ -27,9 +30,35 @@ JoinView::JoinView(ProtocolSocket &ps, std::string line_match, QWidget *parent) 
 }
 
 void JoinView::on_btnBoxJoin_accepted() {
-  QListWidget *matchList = findChild<QListWidget*>("matchList");
-  std::string selectedMatch = matchList->selectedItems()[0]->text().toStdString();
-  std::cout << "Me uno a " << selectedMatch << "\n";
+    QListWidget *matchList = findChild<QListWidget*>("matchList");
+
+    std::vector<uint8_t> buffer(4096);
+    std::string server_match_answer("ERROR");
+    while (server_match_answer.substr(0,5) == "ERROR") {
+        std::cout << "Write match name..." << "\n";
+        std::string selectedMatch = matchList->selectedItems()[0]->text().toStdString();
+        std::string match_name = selectedMatch.substr(0, selectedMatch.find(" "));
+        std::cout << "Me uno a " << match_name << "\n";
+        ps.send(match_name);
+        ps.receive(buffer);
+        server_match_answer.assign(reinterpret_cast<const char *>(buffer.data()), buffer.size());
+        buffer.clear(); buffer.resize(4096);
+        std::cout << server_match_answer;
+    }
+
+    std::string server_username_answer("ERROR");
+    while (server_username_answer.substr(0,5) == "ERROR") {
+        std::cout << "Write your username..." << "\n";
+        QLineEdit *usrTxtIn = findChild<QLineEdit*>("usrTxtIn");
+        std::string username = usrTxtIn->text().toStdString();
+        std::cout << "EL usuario " << username << "\n";
+        ps.send(username);
+        ps.receive(buffer);
+        server_username_answer.assign(reinterpret_cast<const char *>(buffer.data()), buffer.size());
+        buffer.clear(); buffer.resize(4096);
+        std::cout << server_username_answer;
+    }
+    this->close();
 }
 
 JoinView::~JoinView() {
