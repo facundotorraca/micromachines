067f108700b9bd748602e93f95921d9d948d701d
Javier <javiferreyra97@gmail.com>
2019-10-25 15:15:48 -0300

    ahora se puede redimensionar la pantalla sin cambiar el aspecto

--- src/client/Camera.cpp
+++ src/client/Camera.cpp
@@ -7,6 +7,7 @@ Camera::Camera() :
     posy(0),
     width(WIDTH_SCREEN),
     height(HEIGHT_SCREEN),
+    scale(1),
     t_factory(nullptr),
     renderer(nullptr),
     window(nullptr)
@@ -21,48 +22,36 @@ void Camera::update(int32_t posx, int32_t posy) {
 }
 
 void Camera::draw() {
-    int32_t w, h;
-    SDL_GetWindowSize(window, &w, &h);
-    width = w;
-    height = h;
     SDL_RenderPresent(renderer);
 }
 
 void Camera::clear() {
     SDL_RenderClear(renderer);
+    SDL_GetWindowSize(window, &width, &height);
+    scale = (((double)width/1920)+((double)height/1080))/2;
 }
 
 void Camera::drawCar(int32_t x, int32_t y, int32_t rot) {
-    int32_t px = (width / 2) + x - this->posx;
-    int32_t py = (height / 2) + y - this->posy;
-    SDL_Rect dst{px, py, (int) (CAR_WIDTH * METER_TO_PIXEL),
-                     (int) (CAR_HEIGHT * METER_TO_PIXEL)};
-    if (isInCamera(px, py, dst.w, dst.h)) {
-        SDL_RenderCopyEx(renderer, t_factory.getCarTexture(), nullptr, &dst,
-                         rot,
-                         nullptr, SDL_FLIP_NONE);
-    }
+    copyRender(t_factory.getCarTexture(), x, y, rot,
+            CAR_WIDTH*METER_TO_PIXEL, CAR_HEIGHT*METER_TO_PIXEL);
 }
 
 void Camera::drawTile(int32_t x, int32_t y, int32_t rot, int32_t type) {
-    int32_t px = (width/2) + x - this->posx;
-    int32_t py = (height/2) + y - this->posy;
-    SDL_Rect dst{px, py, (int) (WIDTH_TILE * METER_TO_PIXEL),
-                     (int) (HEIGHT_TILE * METER_TO_PIXEL)};
-    if (isInCamera(px, py, dst.w, dst.h)) {
-        SDL_RenderCopyEx(renderer, t_factory.getTileTexture(type), nullptr, &dst,
-                         rot,
-                         nullptr, SDL_FLIP_NONE);
-    }
+    int32_t wh = TILE_TERRAIN_SIZE*METER_TO_PIXEL;
+    copyRender(t_factory.getTileTexture(type), x, y, rot, wh, wh);
 }
 
 void Camera::drawWheel(int32_t x, int32_t y, int32_t rot) {
-    int32_t px = (width/2) + x - this->posx;
-    int32_t py = (height/2) + y - this->posy;
-    SDL_Rect dst{px, py, (int) (WIDTH_WHEEL * METER_TO_PIXEL),
-                     (int) (HEIGHT_WHEEL * METER_TO_PIXEL)};
-    if (isInCamera(px, py, dst.w, dst.h)) {
-        SDL_RenderCopyEx(renderer, t_factory.getWheelTexture(), nullptr, &dst,
+    copyRender(t_factory.getWheelTexture(), x, y,
+            rot, WIDTH_WHEEL*METER_TO_PIXEL, HEIGHT_WHEEL*METER_TO_PIXEL);
+}
+
+void Camera::copyRender(SDL_Texture* tex, int32_t x, int32_t y, int32_t rot, int32_t w, int32_t h){
+    int32_t px = (width*0.5) + scale*(x - this->posx);
+    int32_t py = (height*0.5) + scale*(y - this->posy);
+    SDL_Rect dst{px, py, (int)((double)w*scale), (int)((double)h*scale)};
+    if (isInCamera(dst.x, dst.y, dst.w, dst.h)) {
+        SDL_RenderCopyEx(renderer, tex, nullptr, &dst,
                          rot,
                          nullptr, SDL_FLIP_NONE);
     }
@@ -70,4 +59,9 @@ void Camera::drawWheel(int32_t x, int32_t y, int32_t rot) {
 
 bool Camera::isInCamera(int x,int y, int w, int h){
     return (((x+w)>=0) && (x<width) && ((y+h)>=0) && (y<height));
+}
+
+Camera::~Camera() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
 }
