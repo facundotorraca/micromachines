17b3d3e64a67e2db20104bb7cee3ce08812a27b9
Alejo <aleacevedo@live.com.ar>
2019-10-26 18:57:54 -0300

    HotFix

--- src/client/main.lua.cpp
+++ src/client/main.lua.cpp
@@ -7,9 +7,8 @@
 #include <common/ProtectedQueue.h>
 
 int main(int argc, char *argv[]) {
-    ProtectedQueue<std::vector<int32_t>> queue(10000);
+    ProtectedQueue<std::unique_ptr<ServerCommand>> queue(10000);
     Bot bot(queue);
     bot.execute();
     auto v = queue.pop();
-    std::cout << v.at(0) << " , " << v.at(1) << std::endl;
 }
