991a89b9c63eb315186e58033f7a831645ad312e
Alejo <aleacevedo@live.com.ar>
2019-10-27 20:24:11 -0300

    Modiy lua script

--- lua/bot.lua
+++ lua/bot.lua
@@ -7,69 +7,72 @@ function init(keysDef, actionsDef)
   actions = actionsDef
 end
 
-function addTile(x, y, value)
-  print("ADD TILE")
-  print("X", x)
-  print("Y", y)
-  print("VALUE", value)
-  if map[x] == nil then map[x] = {} end
-  map[x][y] = value
+function addTile(xi, xf, yi, yf, value)
+  for x=xi, xf do
+    for y=yi, yf do
+      if map[x] == nil then map[x] = {} end
+      map[x][y] = value
+      --print(x, y)
+    end
+  end
 end
 
 function updateCar(newCar)
-  print("UPDATE CAR")
   for key, value in pairs(newCar) do
-    print(key, value)
     car[key] = value
   end
 end
 
-function getDirections(rot)
-  if 337 <= rot and rot < 22 then
+function getDirections(rot_ini)
+  local rot = math.fmod(rot_ini, 360)
+  if 337 <= rot or rot < 22 then
     return 0, 1
   elseif 22 <= rot and rot < 67 then 
-    return 1, 1
+    return -0.5, 0.5
   elseif 67 <= rot and rot < 112 then
-    return 1, 0
+    return -1, 0
   elseif 112 <= rot and rot < 157 then
-    return 1, -1
+    return -0.5, -0.5
   elseif 157 <= rot and rot < 202 then
     return 0, -1
   elseif 202 <= rot and rot < 247 then
-    return -1, -1
+    return 0.5, -0.5
   elseif 247 <= rot and rot < 292 then
-    return -1, 0
+    return 1, 0
   elseif 292 <= rot and rot < 337 then
-    return -1, 1
+    return 0.5, 0.5
   end
+  return 0, 0
 end
 
 function canGo(futureX, futureY)
-  return not (0 > futureX or futureX > 100 or 0 > futureY or futureY > 100)
+  return not (map[futureX][futureY] == nil)
 end
 
 function canTurn(to)
   local dirX, dirY = getDirections(car.rot + to)
-  local futureX = car.posX + (3 * dirX)
-  local futureY = car.posY + (3 * dirY)
+  local futureX, futureY = calcFuture(dirX, dirY)
   return canGo(futureX, futureY)
 end
 
 function canTurnLeft()
-  return canTurn(45)
-end
-
-function canTurnRight()
   return canTurn(-45)
 end
 
+function canTurnRight()
+  return canTurn(45)
+end
+
+function calcFuture(dirX, dirY)
+  local futureX = car.posX + (100 * dirX)
+  local futureY = car.posY + (100 * dirY)
+  return futureX, futureY
+end
 
 
 function decide()
-  print("HOLA DESDE DECIDE")
   local dirX, dirY = getDirections(car.rot)
-  local futureX = car.posX + (3 * dirX)
-  local futureY = car.posY + (3 * dirY)
+  local futureX, futureY = calcFuture(dirX, dirY)
   if not canGo(futureX, futureY) then
     if canTurnLeft() then
       return actions.press, keys.left
