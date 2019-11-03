993165f8ed9422720443c036d553243f1841c368
Javier <javiferreyra97@gmail.com>
2019-11-03 20:36:21 -0300

    mejora de fps

--- src/client/ThreadDrawer.cpp
+++ src/client/ThreadDrawer.cpp
@@ -12,7 +12,9 @@ void ThreadDrawer::run() {
         auto finish = std::chrono::high_resolution_clock::now();
         float duration = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
         std::cerr << duration << std::endl;
-        if (duration > 16 || duration < 0)
+        if (duration > 16)
+            duration = 16;
+        if (duration < 0)
             duration = 0;
         SDL_Delay(1000.0f/60.0f - duration);
     }
