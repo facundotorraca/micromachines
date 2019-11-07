8a5fe2d110bec9c423139debf7aad15cb1ddaf80
Alejo <aleacevedo@live.com.ar>
2019-11-07 09:27:27 -0300

    Add PlugingsManager class

---
+++ src/server/PlugingsManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alejo on 7/11/19.
+//
+
+#include "PlugingsManager.h"
---
+++ src/server/PlugingsManager.h
@@ -0,0 +1,14 @@
+//
+// Created by alejo on 7/11/19.
+//
+
+#ifndef MICROMACHINES_PLUGINGSMANAGER_H
+#define MICROMACHINES_PLUGINGSMANAGER_H
+
+
+class PlugingsManager {
+
+};
+
+
+#endif //MICROMACHINES_PLUGINGSMANAGER_H
