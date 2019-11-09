df4093ef06994bb9e13e9d803bf51f648a9ce8b1
Ale Acevedo <ale.acevedo@live.com.ar>
2019-11-09 00:22:54 -0300

    Add init and destroy functions and some logic

--- src/plugings/BoostLast.cpp
+++ src/plugings/BoostLast.cpp
@@ -2,22 +2,48 @@
 // Created by alejo on 7/11/19.
 //
 
-#include <server/PlugingsManager.h>
-#include <model/Vehicle/Car.h>
+#include <model/DTO_Info.h>
+#include <iostream>
 
-void run(PlugingsParams *params) {
-    std::cout << "RUN PLUGINNG" << std::endl;
-    std::cout << params->car_len << std::endl;
-    for (size_t ind = 0; ind < params->car_len; ind++) {
-        Car oneCar(params->cars[ind]);
-        std::cout << oneCar.get_max_life() << std::endl;
-        oneCar.set_max_life(5);
-        params->cars[ind] = oneCar.get_specs();
+typedef struct self {
+    uint32_t step;
+    int32_t last_boosted_id;
+} BoostLast_t;
+
+void run(BoostLast_t *self, DTO_Info *params) {
+    self->step++;
+    std::cout << "Running BOST LAST pluging " << self->step << std::endl;
+    size_t last = 0;
+    int32_t last_id = 0;
+    float *last_speed = NULL;
+    for (size_t ind = 0; ind < params->cars; ind++) {
+        if (params->car_info[ind].position > last) {
+            last = params->car_info[ind].position;
+            last_speed = &(params->car_info[ind].specs.max_forward_speed);
+            last_id = params->car_info[ind].ID;
+        }
+        if (params->car_info[ind].ID == self->last_boosted_id) {
+            params->car_info[ind].specs.max_forward_speed = 0;
+        }
+    }
+    if (self->last_boosted_id != last_id) {
+        *last_speed = *last_speed * 2;
+        self->last_boosted_id = last_id;
     }
 }
 
 extern "C" {
-    void execute(PlugingsParams *params) {
-        run(params);
+    void* init() {
+        BoostLast_t *self = (BoostLast_t*) malloc(sizeof(BoostLast_t));
+        self->step = 0;
+        self->last_boosted_id = -1;
+        return self;
     }
+    void execute(BoostLast_t *self, DTO_Info *params) {
+        run(self, params);
+    }
+    void destroy(BoostLast_t *self) {
+        free(self);
+    }
+
 }
