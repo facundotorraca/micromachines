6fa469ed7c4af0412b9d7e0dc2c75b768673d067
Ale Acevedo <ale.acevedo@live.com.ar>
2019-11-17 11:31:56 -0300

    Init bot if the box is checked

--- src/client/GameMain.h
+++ src/client/GameMain.h
@@ -5,9 +5,10 @@
 
 class GameMain {
     ProtocolSocket socket;
+    bool use_bot;
 
     public:
-        explicit GameMain(ProtocolSocket &socket);
+        explicit GameMain(ProtocolSocket &socket, bool use_bot);
 
         void start();
 };
