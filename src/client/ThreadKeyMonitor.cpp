651aee297df8ebee3f0b48a568be2323269d53bf
Javier <javiferreyra97@gmail.com>
2019-11-26 03:44:44 -0300

    joystick

--- src/client/ThreadKeyMonitor.cpp
+++ src/client/ThreadKeyMonitor.cpp
@@ -3,10 +3,42 @@
 //
 
 #include "ThreadKeyMonitor.h"
-#include <SDL2/SDL.h>
 #include "KeyCommands/KeyCommand.h"
 
 ThreadKeyMonitor::ThreadKeyMonitor(Scene &scene) : Thread(), scene(scene) {}
+
+void ThreadKeyMonitor::tryHandleJoystick(SDL_Event& event){
+    SDL_Keycode key;
+    SDL_EventType downorup;
+     
+    if (event.type == SDL_JOYBUTTONDOWN){
+        downorup = SDL_KEYDOWN;
+    } else if (event.type == SDL_JOYBUTTONUP) {
+        downorup = SDL_KEYUP;
+    }
+
+    if (event.type == SDL_JOYHATMOTION){
+        if (event.jhat.value == SDL_HAT_LEFT || event.jhat.value == SDL_HAT_LEFTUP || event.jhat.value == SDL_HAT_LEFTDOWN){
+            downorup = SDL_KEYDOWN;
+            key = SDLK_LEFT;
+        } else if (event.jhat.value == SDL_HAT_RIGHT || event.jhat.value == SDL_HAT_RIGHTUP || event.jhat.value == SDL_HAT_RIGHTDOWN){
+            downorup = SDL_KEYDOWN;
+            key = SDLK_RIGHT;
+        } else if (event.jhat.value == SDL_HAT_UP || event.jhat.value == SDL_HAT_CENTERED || event.jhat.value == SDL_HAT_DOWN){
+            downorup = SDL_KEYUP;
+            key = SDLK_RIGHT;
+        }
+    } else if (event.type == SDL_JOYBUTTONDOWN || event.type == SDL_JOYBUTTONUP) {
+        switch (event.jbutton.button){
+            case 2: key = SDLK_UP; break;
+            case 3: key = SDLK_DOWN; break;
+            default: return;
+        }
+    } else {
+        return;
+    }
+    this->scene.handleKeyEvent(key, downorup);
+}
 
 void ThreadKeyMonitor::run() {
     SDL_Event event;
@@ -19,13 +51,11 @@ void ThreadKeyMonitor::run() {
         }
 
         if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) {
-            //scene.handleKeyEvent(keyEvent.keysym.sym,
-            //                     (SDL_EventType)(event.type));
             auto command = KeyCommand::create(scene, keyEvent.keysym.sym,
                                               (SDL_EventType) event.type);
-            //this->running = scene.handleKeyEvent(keyEvent.keysym.sym,
-            //                                     (SDL_EventType)(event.type));
             this->running = command->apply();
+        } else {
+            tryHandleJoystick(event);
         }
     }
 }
